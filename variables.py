05ecc024df475be34530dca8f33007fb2d580ba5
shivbalajiramesh <shivbalaji.ramesh@cesltd.com>
2024-09-16 16:35:07 +0530

    Adding 2 files

---
+++ variables.py
@@ -0,0 +1,7 @@
+a = 10
+b = 10
+c = "Shiv"
+d = a + b
+print(type(d))
+
+print(type(c))
---
+++ while.py
@@ -0,0 +1,21 @@
+i = 0
+while(i<6):
+    print(i)
+    i = i + 1
+
+print("\nNew Program\n")
+
+i = 10
+while(i<201):
+    print(i)
+    i = i + 10
+
+print("\nNew Program\n")
+
+i = 5
+fact = 1
+
+while(i>0):
+    fact = fact * i
+    i = i -1
+print(fact)
